37ebd986197fc5bec240d7e7988ff271c0e47f2a
VyomPandya <22it157@charusat.edu.in>
2025-04-23 22:42:02 +0530

    made some changes

--- openrouter.js
+++ openrouter.js
@@ -2,6 +2,17 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'https://vyompandya.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
